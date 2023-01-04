71b386b8b4f440f96191f293268acb98c47507b7
gediyanikita@gmail.com <nikitagediya>
2023-01-04 14:23:43 +0530

    done

--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
-import Express from "express";
-const app = Express();
+import express from "express";
+// const mysql = require('mysql');
+
+const app = express();
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
 import postRoutes from "./routes/posts.js"
 import commentRoutes from "./routes/comments.js"
 import likeRoutes from "./routes/likes.js"
 
+//middlewares
+app.use(express.json())
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/comments", commentRoutes)
 app.use("/api/likes", likeRoutes)
+
 app.listen(8800, () => {
-    console.log("API working!")
-})
+    console.log("API working!");
+});
